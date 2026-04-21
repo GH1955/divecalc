b37e9a1b7c945eac84cacf367cc444ebbd068e90
GH1955 <guenther@hackl.wien>
2026-04-21 12:25:39 +0200

    Fix absolute paths for Vercel

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.static("."));
-app.get(/(.*)/, (req, res) => res.sendFile("index.html", { root: "." }));
+app.use(express.static(path.join(__dirname, ".")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 app.listen(PORT, "0.0.0.0", () => console.log("Dive Calculator listening on " + PORT));
+module.exports = app;
